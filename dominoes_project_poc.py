77c260906cb09200585ee1c762d59f14c0254a1c
johnychhantyal2 <jchhantyal@go.stcloudstate.edu>
2022-09-30 17:12:33 -0500

    Initial outline of the project

---
+++ dominoes_project_poc.py
@@ -0,0 +1,16 @@
+# Import necessary libraries
+
+# Define Classes
+# CDominoes - contains the data structure with pieces
+# CRandom - as sorting approach for the sequence that the domino pieces will be shown/picked
+# CPlayer - to select a randomly picked and sequentially show the selected pieces
+# CTable - to show/display the sorted pieces
+
+# Game Setup
+# Select the tiles
+# Select the player to play first
+# Display messages to welcome players and direct them to play
+
+# Implement Gameplay & Winning Conditions
+
+# Diplay who won and the final snake
